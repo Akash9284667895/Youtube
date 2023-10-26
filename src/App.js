0c1c0f4de31be8ea94c2d40cc5c218b1b2c81e13
Akash Lande <akashlande97@gmail.com>
2023-10-26 14:18:21 +0530

    navigation links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { Provider } from 'react-redux';
 import './App.css';
 import Body from './Component/Body';
+import {createBrowserRouter} from "react-router-dom"
 import Header from './Component/Header';
 import appStore from './utils/appStore';
 
 function App() {
+
+  const appRouter = createBrowserRouter([{
+
+  }])
   return (
     <div>
       <Provider store={appStore}>
---
+++ src/Component/WatchPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WatchPage = () => {
+  return (
+    <div>
+      <h1>WatchPage</h1>
+    </div>
+  )
+}
+
+export default WatchPage
