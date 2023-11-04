efb587ee6b1ec8c75b8f159a04ac25647f993f8d
Akash Lande <akashlande97@gmail.com>
2023-11-04 17:14:28 +0530

    store

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appSlice from "./appSlice";
+import searchSlice from "./searchSlice";
 
 const appStore = configureStore({
   reducer: {
     app: appSlice,
+    search:searchSlice,
   },
 });
 
